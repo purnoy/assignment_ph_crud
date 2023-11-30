29e1f246927c2cc724a5471611771ba4c6794b3f
Purnot <purnoy.cc@gmail.com>
2023-11-30 18:27:17 +0600

    some more correction

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { UserRouter } from './modules/user/user.routes';
 
@@ -9,5 +9,20 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/users', UserRouter);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to the User API ');
+});
+
+app.all('*', (req: Request, res: Response) => {
+    res.json({
+        success: false,
+        message: 'Invalid url',
+        error: {
+            code: 404,
+            description: 'This is not an authorized url',
+        },
+    });
+});
 
 export default app;
